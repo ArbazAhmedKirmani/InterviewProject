22f8346b04c465c4af4cb7ebd5628cc9a0eddcdc
ArbazAhmedKirmani <arbazahmedkirmani@gmail.com>
2021-09-22 17:05:14 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+
+  const [mammels, setMammels] = useState([])
+
+
+  useEffect(() => {
+    let array = []
+    const variable = document.querySelectorAll('.animal')
+
+
+    variable.forEach(element => {
+      const object = { name: element.querySelector('h2').innerHTML, value: element.querySelector('.mammal-value').innerHTML }
+      if (object.value === 'true') array.push(object)
+    });
+    setMammels(array)
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>All animals</h1>
+      <section className='all-animals'>
+        <div className='animal'>
+          <h2>Dog</h2>
+          <p>mammal: <span className='mammal-value'>true</span></p>
+        </div>
+        <div className='animal'>
+          <h2>Snake</h2>
+          <p>mammal: <span className='mammal-value'>false</span></p>
+        </div>
+        <div className='animal'>
+          <h2>Cheetah</h2>
+          <p>mammal: <span className='mammal-value'>true</span></p>
+        </div>
+        <div className='animal'>
+          <h2>Turtle</h2>
+          <p>mammal: <span className='mammal-value'>false</span></p>
+        </div>
+        <div className='animal'>
+          <h2>Frog</h2>
+          <p>mammal: <span className='mammal-value'>false</span></p>
+        </div>
+        <div className='animal'>
+          <h2>Cat</h2>
+          <p>mammal: <span className='mammal-value'>true</span></p>
+        </div>
+        <div className='animal'>
+          <h2>Badger</h2>
+          <p>mammal: <span className='mammal-value'>true</span></p>
+        </div>
+      </section>
+      <h1>Mammals</h1>
+      <section id='only-mammals'></section>
+
+      <p>------------------------------------------------</p>
+
+      {mammels.map((data, i) => (
+        <h3 key={i}>{data.name} - {data.value}</h3>
+      ))}
+
     </div>
   );
-}
+};
+
 
 export default App;
